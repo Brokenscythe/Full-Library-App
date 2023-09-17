598d97597e07887506f1218548dd20f1c9b574c2
Dusan_Ivanovic <dusanivanoviri@gmail.com>
2023-09-17 20:02:51 +0200

    Added functions for route handlers

--- src/controllers/auth-controllers/authController.ts
+++ src/controllers/auth-controllers/authController.ts
@@ -1,14 +1,19 @@
+import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
+import User from "../../models/userModel";
+
 const db = new PrismaClient();
 
-export interface User {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
+export async function getRegister(req: Request, res: Response): Promise<void> {
+  res.render("auth/register");
 }
+export async function getLogIn(req: Request, res: Response): Promise<void> {
+  res.render("auth/login");
+}
+export async function signup(req: Request, res: Response): Promise<void> {}
+export async function login(req: Request, res: Response): Promise<void> {}
 
 export const getUser = async (email: string) => {
   return await db.user.findUnique({
@@ -26,10 +31,15 @@ export const getUserById = async (id: string) => {
   });
 };
 
-export const createUser = async (name: string, email: string, password: string) => {
-  const hashedPassword = bcrypt.hashSync(password, 8);
+// export const createUser = async (name: string, email: string, password: string) => {
+//   const hashedPassword = bcrypt.hashSync(password, 8);
 
-  const user = await db.user.create({
-    data: { name, email, password: hashedPassword },
-  });
-};
+//   const user = await db.user.create({
+//     data: {
+//       name,
+//       email,
+//       password: hashedPassword,
+//       // You may need to provide default values or omit fields that are not required.
+//     },
+//   });
+// };
